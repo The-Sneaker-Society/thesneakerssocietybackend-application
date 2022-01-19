b30a013610ab04d085fcaf6a9310ce3b16f7788b
Rabin Adhikari <48793377+rabin1995@users.noreply.github.com>
2022-01-19 00:51:28 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+const dotenv = require('dotenv')
+dotenv.config();
+// console.log(process.env);
+
+var con = mysql.createConnection ({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
+app.use(cors());
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended:true }));
+
+// app.get("/", (req, res) => {
+//     const sqlSelect = "INSERT INTO myproject (id, Name, Email, Password) VALUES ('11', 'Rolan Tan', 'tanrolan@gmail.com', 'hfggrgrg')";
+//     db.query(sqlSelect, (err, result) => {
+//         res.send("hello World");
+//     });
+// });
+app.get("/", (req, res) => {
+    const sqlSelect = "INSERT INTO Users_Table (ID, First_Name, Last_Name, Email, created_at, updated_at, confirmed, verified, Firebase_id ) VALUES ('103', 'Tom', 'Ruild', 'tomruild@gmail.com', 'Wednesday, December 22, 2021', 'Wednesday, December 22, 2021', 'TRUE', 'FALSE', '101')";
+    con.query(sqlSelect, (err, result) => {
+        res.send("Hello World");
+    });
+});
+// app.post("/api/insert", (req, res) => {
+//     const id = req.body.id;
+//     const Name = req.body.Name;
+//     const Email = req.body.Email;
+//     const Password = req.body.Password;
+
+    // const sqlInsert = "INSERT INTO myproject (id, Name, Email, Password) VALUES (?,?,?,?)";
+    // db.query(sqlInsert, [id, Name, Email, Password], (err, result)=> {
+    //     console.log(result);
+// });
+// });
+app.listen(3005, () => {
+    console.log("running on port 3005");
+});
