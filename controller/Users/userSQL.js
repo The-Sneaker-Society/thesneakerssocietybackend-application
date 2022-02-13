2619cb0d1803b03e10ff196059386930b7ec99f4
Alanis Yates <yatesalanis@gmail.com>
2022-02-12 19:52:43 -0500

    updated demo

--- controller/Users/userSQL.js
+++ controller/Users/userSQL.js
@@ -1,5 +1,5 @@
 export const creatUSerStatement = (dataTosend) => {
-    return `SELct.... ${dataTosend.email}`
+    return `INSERT INTO Users_Table (ID, First_Name, Last_Name, Email, created_at, updated_at, confirmed, verified, Firebase_id) VALUES ('104', ${datatosend.firstName}, 'Tan', 'tanrolan@gmail.com', 'Wednesday, December 22, 2021', 'Wednesday, December 22, 2021', 'TRUE', 'FALSE', '102'`
 }
 
 export const updateUSerSQLStatement = (datatosend) => {
