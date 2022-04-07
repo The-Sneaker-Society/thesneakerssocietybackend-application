77b8988f7d7f9c981d0439c3dd186210de6cfa50
Rabin Adhikari <48793377+rabin1995@users.noreply.github.com>
2022-04-06 20:13:59 -0400

    Update Friends_List_Table.js

--- Friends_List_Table.js
+++ Friends_List_Table.js
@@ -1,111 +1,75 @@
-const sql = require("./db.js");
-// constructor
-const Friends_List_Table = function(Friends_List_Table) {
-  this.ID = Friends_List_Table.ID;
-  this.First_Name = Friends_List_Table.First_Name;
-  this.Last_Name = Friends_List_Table.Last_Name;
-  this.Email = Friends_List_Table.Email;
-  this.added_at = Friends_List_Table.added_at;
-  this.Firebase_id = Friends_List_Table.Firebase_id;
-};
-Friends_List_Table.create = (newFriends_List_Table, result) => {
-  sql.query("INSERT INTO Friends_List_Table SET ?", newFriends_List_Table, (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
-    console.log("created Friends_List_Table: ", { ID: res.insertID, ...newFriends_List_Table });
-    result(null, { ID: res.insertID, ...newFriends_List_Table });
-  });
-};
-Friends_List_Table.findByID = (ID, result) => {
-  sql.query(`SELECT * FROM Friends_List_Table WHERE ID = ${ID}`, (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
-    if (res.length) {
-      console.log("found Friends_List_Table: ", res[0]);
-      result(null, res[0]);
-      return;
-    }
-    // not found Friends_List_Table with the ID
-    result({ kind: "not_found" }, null);
-  });
-};
-Friends_List_Table.getAll = (First_Name, result) => {
-  let query = "SELECT * FROM Friends_List_Table";
-  if (First_Name) {
-    query += ` WHERE First_Name LIKE '%${First_Name}%'`;
-  }
-  sql.query(query, (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(null, err);
-      return;
-    }
-    console.log("Friends_List_Table: ", res);
-    result(null, res);
-  });
-};
-Friends_List_Table.getAllPublished = result => {
-  sql.query("SELECT * FROM Friends_List_Table WHERE ID=1", (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(null, err);
-      return;
-    }
-    console.log("Friends_List_Table: ", res);
-    result(null, res);
-  });
-};
-Friends_List_Table.updateByID = (ID, Friends_List_Table, result) => {
-  sql.query(
-    "UPDATE Friends_List_Table SET First_Name = ?, Last_Name = ?, added_at = ?, Firebase_id = ? WHERE ID = ?",
-    [Friends_List_Table.First_Name, Friends_List_Table.Last_Name, Friends_List_Table.added_at,Friends_List_Table.Firebase_id, ID],
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      if (res.affectedRows == 0) {
-        // not found Friends_List_Table with the ID
-        result({ kind: "not_found" }, null);
-        return;
-      }
-      console.log("updated Firebase_id: ", { ID: ID, ...Firebase_id });
-      result(null, { ID: ID, ...Firebase_id });
-    }
-  );
-};
-Friends_List_Table.remove = (ID, result) => {
-  sql.query("DELETE FROM Firebase_id WHERE ID = ?", ID, (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(null, err);
-      return;
-    }
-    if (res.affectedRows == 0) {
-        // not found Friends_List_Table with the ID
-        result({ kind: "not_found" }, null);
-        return;
-      }
-      console.log("Deleted Firebase_id with ID: ", ID);
-      result(null, res);
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const db = mysql.createPool({
+    host: "sneakerssociety.cursd5wkphyn.us-east-2.rds.amazonaws.com",
+    user: "sneakerssociety",
+    password: "amazonrds",
+    database: "thesneakerssocietydatabase"
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/api/get", (req, res) => {
+    const sqlGet = "SELECT * FROM Friends_List_Table";
+    db.query(sqlGet, (error, result) => {
+        res.send(result);
+    })
+})
+
+// app.get("/api/posts", (req, res) => {
+//     // const {ID, First_Name, Last_Name, Email, created_at, updated_at, confirmed, verified, Firebase_id} = req.body;
+//     const sqlInsert = "INSERT INTO Friends_List_Table (ID, First_Name, Last_Name, Email, created_at, updated_at, confirmed, verified, Firebase_id) VALUES (104, 'Tanny', 'Pane', 'tannypane@gmail.com', 'Monday, December 6, 2021', 'Monday, December 6, 2021', 'TRUE', 'FALSE', 104)";
+//     db.query(sqlInsert, (err, result) => {
+//         res.send("Hello World");
+//         // }
+//     });
+// });
+
+app.get("/api/remove/ID", (req, res) => {
+    const {ID} = req.params;
+    const sqlRemove = "DELETE FROM Friends_List_Table WHERE ID = 10";
+    db.query(sqlRemove, ID, (error, result) => {
+        res.send(result);
     });
-  };
-  Friends_List_Table.removeAll = result => {
-    sql.query("DELETE FROM Firebase_id", (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      console.log(`deleted ${res.affectedRows} Firebase_id`);
-      result(null, res);
+});
+
+app.get("/api/post", (req, res) => {
+    const sqlInsert = "INSERT INTO Friends_List_Table (ID, First_Name, Last_Name, Email, created_at, updated_at, confirmed, verified, Firebase_id) VALUES (11, 'Tanny', 'Pane', 'tannypane@gmail.com', 'Monday, December 6, 2021', 'Monday, December 6, 2021', 'TRUE', 'FALSE', 11)";  
+    db.query(sqlInsert, (err, result) => {
+    // console.log("error", error);
+    // console.log("result", result);
+    res.send("Hello World");
+});
+});
+
+app.get("/api/get/:ID", (req, res) => {
+    const {ID} = req.params;
+    const sqlGet = "SELECT * FROM Friends_List_Table where ID=10";
+    db.query(sqlGet, ID, (error, result) => {
+        if (error) {
+            console.log(error);
+        }
+        res.send(result);
     });
-  };
-  module.exports = Tutorial;
+});
+
+app.get("/api/put/:ID", (req, res) => {
+    const {ID} = req.params;
+    const sqlGet = "SELECT * FROM Friends_List_Table";
+    db.query(sqlGet, ID, (error, result) => {
+        if (error) {
+            console.log(error);
+        }
+        res.send(result);
+    });
+});
+
+app.listen(8000, () => {
+    console.log("Server is running on port 8000");
+})
